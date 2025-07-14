f391cb3bc6488d642eef49b18f4337ee9ac0e57b
Haunes <156840643+Haunes@users.noreply.github.com>
2025-07-14 21:57:13 +0000

    Added docstring constants.py

--- ui/constants.py
+++ ui/constants.py
@@ -1,3 +1,13 @@
+"""
+ui/constants.py
+
+This module defines constant values for the Streamlit UI:
+
+- PLATFORMS: a list of platform names (e.g., 'SIMIT', 'FENIX', etc.) that
+  are enabled based on the available parsers in core.PARSERS.
+- TITLE: the page title displayed at the top of the Streamlit application.
+"""
+
 # ui/constants.py
 from core import PARSERS
 
